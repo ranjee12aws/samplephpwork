9e31bb759a01fcc4ed9c59b19dbb79754032bde0
ranjee12aws <42106950+ranjee12aws@users.noreply.github.com>
2018-08-05 11:42:06 +0530

    UpdatedPendingApproval

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>DevOps Congrats this is Ranjeth... Welcome to the world of AWS</h1>
+<h1>Congrats this is Ranjeth... Welcome to the world of AWS</h1>
 
 <?php
 echo "version :Rev Ver";
